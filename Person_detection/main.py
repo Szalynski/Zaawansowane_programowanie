1a06389da6e0ea400a0e88a480e48645e68b22de
unknown <m.szalynski@op.pl>
2022-12-20 14:45:43 +0100

    Finished

--- Person_detection/main.py
+++ Person_detection/main.py
@@ -1,7 +1,5 @@
 from Functions import loadImage as lI
 from VideoFunctions import PlayVideo as pv
-import cv2 as cv
-import numpy as np
 
 name1 = 'Photos/photo3.jpg'
 name2 = 'Photos/photo4.jpg'
@@ -10,13 +8,13 @@ name4 = 'Photos/photo5.jpg'
 name5 = 'Photos/photo6.jpg'
 
 lI.load(name1)
-#lI.load(name2)
+lI.load(name2)
 #lI.load(name4)
 #lI.load(name5)
 
 #pv.load('Videos/video2.mp4')
 #pv.load('Videos/video3.mp4')
-#pv.load('Videos/video4.mp4')
-#pv.load(name3)
+pv.load('Videos/video4.mp4')
+pv.load(name3)
 
 
